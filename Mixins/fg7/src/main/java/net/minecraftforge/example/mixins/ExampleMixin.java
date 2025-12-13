da1e4d723e9ba59f2e40324da224c6631c7fd1cd
LexManos <LexManos@gmail.com>
2025-12-12 22:12:27 -0800

    Add Mixin Example

---
+++ Mixins/fg7/src/main/java/net/minecraftforge/example/mixins/ExampleMixin.java
@@ -0,0 +1,14 @@
+package net.minecraftforge.example.mixins;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Overwrite;
+
+import net.minecraft.client.main.Main;
+
+@Mixin(Main.class)
+public class ExampleMixin {
+	@Overwrite
+	public static void main(String[] args) {
+		System.out.println("Overwrote main!");
+	}
+}
