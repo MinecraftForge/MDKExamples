03814a03f6dc580ebe7ecebc1dd5810560ebd9e2
LexManos <LexManos@gmail.com>
2025-12-13 11:20:27 -0800

    Add JarJar Example

---
+++ JarJar/fg7/src/main/java/net/minecraftforge/example/ExampleMod.java
@@ -0,0 +1,10 @@
+package net.minecraftforge.example;
+
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+
+@Mod("examplemod")
+public final class ExampleMod {
+    public ExampleMod(FMLJavaModLoadingContext context) {
+    }
+}
