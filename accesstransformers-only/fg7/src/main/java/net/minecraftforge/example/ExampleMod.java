b2244009aae9ce651234f1090323181d7c6cf401
LexManos <LexManos@gmail.com>
2026-01-26 20:26:51 -0800

    Fix AccessTransformer failing to compile due to not transforming ROIMFS. The example of how to use AccessTransformer plugin directly is in the AT repo

--- accesstransformers-only/fg7/src/main/java/net/minecraftforge/example/ExampleMod.java
+++ accesstransformers-only/fg7/src/main/java/net/minecraftforge/example/ExampleMod.java
@@ -3,12 +3,10 @@ package net.minecraftforge.example;
 import net.minecraft.client.Minecraft;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
-import net.minecraftforge.roimfs.GlobUtil;
 
 @Mod("examplemod")
 public final class ExampleMod {
     public ExampleMod(FMLJavaModLoadingContext context) {
     	Minecraft.LOGGER.info("Able to access a private field");
-    	GlobUtil.convertGlobToRegex("test");
     }
 }
