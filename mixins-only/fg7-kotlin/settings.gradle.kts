349686322e9033319e55530371bb5adc306082f5
Angelina Michalek <ianthisawesomee@gmail.com>
2026-03-02 01:06:50 -0500

    Minor tweaks

--- mixins-only/fg7-kotlin/settings.gradle.kts
+++ mixins-only/fg7-kotlin/settings.gradle.kts
@@ -2,4 +2,4 @@ plugins {
     id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0"
 }
 
-rootProject.name = "mdkexamples-mixins-fg7"
+rootProject.name = "mdkexamples-mixins-fg7-kotlin"
