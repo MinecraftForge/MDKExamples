017184c0de0b09aaa637d7d5dd591ca3b6d79c88
LexManos <LexManos@gmail.com>
2026-03-09 03:02:49 -0700

    Add mixin obfuscation example

---
+++ mixins-obfuscation/fg7/src/main/java/net/minecraftforge/example/mixins/ExampleMixin.java
@@ -0,0 +1,20 @@
+package net.minecraftforge.example.mixins;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Overwrite;
+
+import net.minecraft.client.main.Main;
+
+@Mixin(Main.class)
+public class ExampleMixin {
+    @Overwrite
+    public static void main(String[] args) {
+        System.out.println("Overwrote main!");
+    }
+    
+    
+    @Overwrite
+    private static boolean stringHasValue(String value) {
+        return false;
+    }
+}
